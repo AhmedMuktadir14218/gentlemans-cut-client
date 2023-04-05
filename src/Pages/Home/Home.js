653cebe6fa6bec89212468dadd3c9e2901bb595a
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-04-05 22:29:00 +0600

    first commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../Shared/Footer';
+import Navber from '../Shared/Navber';
+
+const Home = () => {
+    return (
+        <div>
+            <Navber></Navber>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Shared/Navber.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navber = () => {
+    return (
+        <div>
+            Footer
+        </div>
+    );
+};
+
+export default Navber;
