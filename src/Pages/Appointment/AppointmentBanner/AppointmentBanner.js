61bf28f4a4f44e9235a2c29cbdf06943dfa6af5e
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-04-27 13:36:33 +0600

    commit

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -2,30 +2,37 @@ import React from 'react';
 
 import { DayPicker } from 'react-day-picker';
 import AppointmentImg from '../../../images/allef-vinicius-IvQeAVeJULw-unsplash.jpg';
-const AppointmentBanner = ({selectedDate, setSelectedDate}) => {
+// import styles from 'react-day-picker/dist/style.module.css';
+import { format } from 'date-fns';
+// const AppointmentBanner = ({selectedDate, setSelectedDate}) => {
+    const AppointmentBanner = () => {
+    const [selectedDate, setSelectedDate] = React.useState(new Date());
 
-    const css = {"--rdp-cell-size": "40px",
-    "--rdp-accent-color": "#0000ff",
-    "--rdp-background-color": "#ff0000",
-    /* Switch to dark colors for dark themes */
-    "--rdp-accent-color-dark": "#3003e1",
-    "--rdp-background-color-dark": "#ff0000",
-    /* Outline border for focused elements */
-    "--rdp-outline": "2px solid var(--rdp-accent-color)",
-    /* Outline border for focused and selected elements */
-    "--rdp-outline-selected": "2px solid rgba(0, 0, 0, 0.75)"}
+    // const classNames: abcd = {
+    //     ...styles,
+    //     head: 'style_root__Vd1RM'
+    //   };
+      const css = `
+      .style_root__Vd1RM { --rdp-background-color: #ff0000;
+         --rdp-accent-color: #ff0000;}
+    `;
     
     return (
         <header className='my-6'>
             <div className="hero">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src={AppointmentImg} alt="dentist chair" className="max-w-sm rounded-lg shadow-2xl" />
-                    <div style={css} className='mr-6 sm:w-full'>
-                        <DayPicker 
+                    <div className='mr-6 sm:w-full'>
+{/* <style>{`.style_root__Vd1RM { --rdp-background-color: #ff0000; --rdp-accent-color: #ff0000;}`}</style>  */}
+            <style>{css}</style>
+                        <DayPicker  
                             mode='single'
+// classNames={classNames}
                             selected={selectedDate}
                             onSelect={setSelectedDate}
                         />
+                        
+                    <p>You have selected date: {format(selectedDate,'PP')}</p>
                     </div>
                 </div>
             </div>
@@ -34,3 +41,37 @@ const AppointmentBanner = ({selectedDate, setSelectedDate}) => {
 };
 
 export default AppointmentBanner;
+
+// import { format } from 'date-fns';
+// import { DayPicker } from 'react-day-picker';
+
+// import React from 'react';
+// import styles from 'react-day-picker/dist/style.module.css';
+
+// const AppointmentBanner = () => {
+//     const [selected, setSelected] = React.useState(new Date());
+
+//     const classNames: classNames = {
+//         ...styles,
+//         head: 'style_root__Vd1RM'
+//       };
+
+//     let footer = <p>Please pick a day.</p>;
+//     if (selected) {
+//       footer = <p>You picked {format(selected, 'PP')}.</p>;
+//     }
+//     return (
+//         <div >
+//             <style>{`.style_root__Vd1RM { --rdp-background-color: #ff0000; --rdp-accent-color: #ff0000;}`}</style>
+//              <DayPicker
+//       mode="single"
+//       classNames={classNames}
+//       selected={selected}
+//       onSelect={setSelected}
+//       footer={footer}
+//     />
+//         </div>
+//     );
+// };
+
+// export default AppointmentBanner;
