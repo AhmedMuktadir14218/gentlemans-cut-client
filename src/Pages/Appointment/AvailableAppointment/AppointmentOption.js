15d5f36e91e00f7a3c8917faddcd330265560ff8
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-04-28 20:00:43 +0600

    commit

---
+++ src/Pages/Appointment/AvailableAppointment/AppointmentOption.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import dummyImg from './../../../images/mostafa-meraji-v7DgWdQL_eE-unsplash.jpg';
+
+const AppointmentOption = ({appointmentOption}) => {
+    const {name,slots,img} =appointmentOption
+    return (
+        <div>
+            <div className="card w-96 bg-neutral shadow-xl">
+  <figure className="px-10 pt-10">
+    <img src={dummyImg} alt="Shoes" className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title">{name}</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default AppointmentOption;
--- src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,10 +1,29 @@
 import { format } from 'date-fns';
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import AppointmentOption from './AppointmentOption';
 const AvailableAppointment = ({selectedDate, setSelectedDate}) => {
+    const [appointmentOptions, setAppointmentOptions] = useState([]);
+    const [treatment, setTreatment] = useState(null);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setAppointmentOptions(data))
+    }, [])
     return (
         <div className='mt-16'>
             <p className='text-center font-bold text-primary'>Available Appointment on {format(selectedDate,'PP')}</p>
+
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
+                {
+                    appointmentOptions.map(option=> <AppointmentOption
+                    key={"_id"} appointmentOption={option} 
+                    >
+
+                    </AppointmentOption>)
+                }
+            </div>
+            {/* <AppointmentOption></AppointmentOption> */}
                     
         </div>
     );
