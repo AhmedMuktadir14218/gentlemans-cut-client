4fe295e5d9b4256af8911954970ad73fb1b59a24
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-05-06 15:24:29 +0600

    module 73

---
+++ src/Context/AuthProvider.js
@@ -0,0 +1,56 @@
+import React, { createContext, useEffect, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile} from 'firebase/auth';
+import app from '../Firebase/firebase.config';
+// import app from '../firebase/firebase.config';
+
+export const AuthContext = createContext();
+const auth = getAuth(app)
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const createUser = (email, password) =>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) =>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const updateUser = (userInfo) =>{
+        return updateProfile(user, userInfo);
+    }
+
+    const logOut = () =>{
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect( () =>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+            console.log('user observing');
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, [])
+
+    const authInfo = {
+        createUser,
+        signIn,
+        updateUser,
+        logOut,
+        user,
+        loading
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import 'react-day-picker/dist/style.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AuthProvider from './Context/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AuthProvider>
     <App />
+    </AuthProvider>
+    
   </React.StrictMode>
 );
 
